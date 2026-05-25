1e0bb1d96e9320c9d0713ab0a45982b1509b21a2
Alexandre <alexandrec@alu.ufc.br>
2026-05-25 16:19:09 -0300

    test: ad test unit month closing

--- app/Domain/Entities/Monthly/MonthlyClosingFactory.php
+++ app/Domain/Entities/Monthly/MonthlyClosingFactory.php
@@ -6,6 +6,21 @@ use App\Domain\Entities\Monthly\ValueObject\Month;
 
 class MonthlyClosingFactory
 {
+    public static function create(
+        int $id,
+        int $enterpriseId,
+        int $month,
+        int $year,
+        bool $closed
+    ): MonthlyClosing {
+        return new MonthlyClosing(
+            $id,
+            $enterpriseId,
+            Month::fromInt($month),
+            $year,
+            $closed
+        );
+    }
 
     public static function createWithIdNull(
         int $enterpriseId,
